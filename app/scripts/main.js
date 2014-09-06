8b07037b0d2b5bff6637b80f8abd40e20767caac
saran.quardz <saran.quardz@gmail.com>
2014-09-07 02:47:06 +0530

    Yeoman Backbone Scaffolding

---
+++ app/scripts/main.js
@@ -0,0 +1,28 @@
+/*global require*/
+'use strict';
+
+require.config({
+    shim: {
+        bootstrap: {
+            deps: ['jquery'],
+            exports: 'jquery'
+        },
+        handlebars: {
+            exports: 'Handlebars'
+        }
+    },
+    paths: {
+        jquery: '../bower_components/jquery/dist/jquery',
+        backbone: '../bower_components/backbone/backbone',
+        underscore: '../bower_components/lodash/dist/lodash',
+        bootstrap: '../bower_components/sass-bootstrap/dist/js/bootstrap',
+        handlebars: '../bower_components/handlebars/handlebars'
+    }
+});
+
+require([
+    'backbone',
+    'bootstrap'
+], function (Backbone) {
+    Backbone.history.start();
+});
